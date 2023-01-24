e0f4af03f9c4cea18a79eb207f31f2d998317a98
Natali Kamenkova <natalikamenkova@gmail.com>
2023-01-24 18:59:56 +0200

    header

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li>
+           <img className="mr-10" width={18} height={18} src ="/img/cart.svg"/>
+            <span className="mr-30">1205 руб.</span>              
+          </li>
+          <li>
+            <img width={18} height={18} src ="/img/user.svg"/>
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40 ">
+        <h1>Все кроссовки</h1>
+        ....
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
